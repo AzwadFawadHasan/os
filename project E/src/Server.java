bdf1652bfc47c961d9a44c6d985270a3c68593d9
AzwadFawadHasan2020222 <106537814+AzwadFawadHasan2020222@users.noreply.github.com>
2023-05-10 00:51:09 +0600

    added filesz

---
+++ project E/src/Server.java
@@ -0,0 +1,113 @@
+import java.io.*;
+import java.net.*;
+
+// Server class
+class Server {
+	public static void main(String[] args)
+	{
+		ServerSocket server = null;
+
+		try {
+
+			// server is listening on port 1234
+			server = new ServerSocket(1234);
+			//server.setReuseAddress(true);
+
+			// running infinite loop for getting
+			// client request
+			while (true) {
+
+				// socket object to receive incoming client
+				// requests
+				Socket c = server.accept();
+
+				// Displaying that new client is connected
+				// to server
+				System.out.println("New client connected "+ c.getInetAddress().getHostAddress());
+
+				// create a new thread object
+				ClientHandler clientSock
+					= new ClientHandler(c);
+
+				// This thread will handle the client
+				// separately
+				new Thread(clientSock).start();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (server != null) {
+				try {
+					server.close();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	// ClientHandler class
+	private static class ClientHandler implements Runnable {
+		private final Socket clientSocket;
+
+		// Constructor
+		public ClientHandler(Socket socket)
+		{
+			this.clientSocket = socket;
+		}
+
+		public void run()
+		{
+			PrintWriter out = null;
+			BufferedReader in = null;
+			try {
+					
+				// get the outputstream of client
+				out = new PrintWriter(
+					clientSocket.getOutputStream(), true);
+
+				// get the inputstream of client
+				in = new BufferedReader(
+					new InputStreamReader(
+						clientSocket.getInputStream()));
+
+				String line;
+				while ((line = in.readLine()) != null) {
+
+					// writing the received message from
+					// client
+					System.out.printf(
+						" Sent from the client: %s\n",
+						line);
+                                        if("exit".equals(line)){
+                                            System.out.println("Client Disconnected \n");
+                                            out.println("you are disconnected \n");
+                                        }else{
+					out.println(line);
+                                        }
+                                        
+				}
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				try {
+					if (out != null) {
+						out.close();
+					}
+					if (in != null) {
+						in.close();
+						clientSocket.close();
+					}
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
